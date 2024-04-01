3efb6de603bee0d52224ab14a9680a97154badb1
nidhi-311 <156688561+nidhi-311@users.noreply.github.com>
2024-04-01 09:56:49 -0400

    Add files via upload

---
+++ TripFlix/CSS/text.py
@@ -0,0 +1,14 @@
+print("hi")
+
+print(3+4)
+print('3'+'4')
+print('Nidhi','Bansal',end='*')
+
+
+a=int(input("enter no. 1"))
+b=int(input("enter no.2"))
+print(a+b)
+
+c=input("enter no. 1")
+d=input("enter no.2")
+print(c+d)
